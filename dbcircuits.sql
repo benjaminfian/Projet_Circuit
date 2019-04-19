10207813b6b9d806570cfa82e2c3b5bf8edf2559
yassine semmar <yassine3054@hotmail.com>
2019-04-19 00:08:58 -0400

    Update dbcircuits.sql

--- dbcircuits.sql
+++ dbcircuits.sql
@@ -1,257 +1,396 @@
--- MySQL Workbench Forward Engineering
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le :  mer. 17 avr. 2019 à 19:55
+-- Version du serveur :  5.7.17
+-- Version de PHP :  5.6.30
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
-
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
--- -----------------------------------------------------
--- Schema yassine
--- -----------------------------------------------------
-
--- -----------------------------------------------------
--- Schema yassine
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `yassine` DEFAULT CHARACTER SET latin1 ;
-USE `yassine` ;
-
--- -----------------------------------------------------
--- Table `yassine`.`activite`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `yassine`.`activite` (
-  `idActivite` INT(11) NOT NULL AUTO_INCREMENT,
-  `titreActivite` VARCHAR(100) NOT NULL,
-  `villeActivite` VARCHAR(100) NOT NULL,
-  `descriptionActivite` TEXT NOT NULL,
-  `idJour` INT(11) NOT NULL,
-  PRIMARY KEY (`idActivite`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_unicode_ci;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
--- -----------------------------------------------------
--- Table `yassine`.`circuit`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `yassine`.`circuit` (
-  `idCircuit` INT(11) NOT NULL AUTO_INCREMENT,
-  `titreCircuit` VARCHAR(100) NOT NULL,
-  `descriptionCircuit` TEXT NOT NULL,
-  `dateDebutCircuit` DATE NOT NULL,
-  `dateFinCircuit` DATE NOT NULL,
-  `prixCircuit` DOUBLE NOT NULL,
-  `etat` BIT(1) NOT NULL DEFAULT b'0',
-  `nbPlaceMin` INT(11) NOT NULL,
-  `nbPlaceMax` INT(11) NOT NULL,
-  `nbReservationMax` INT(11) NOT NULL,
-  PRIMARY KEY (`idCircuit`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_unicode_ci;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Base de données :  `dbcircuits`
+--
 
--- -----------------------------------------------------
--- Table `yassine`.`etape`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `yassine`.`etape` (
-  `idEtape` INT(11) NOT NULL AUTO_INCREMENT,
-  `titreEtape` VARCHAR(100) NOT NULL,
-  `descriptionEtape` TEXT NOT NULL,
-  `idCircuit` INT NOT NULL,
-  PRIMARY KEY (`idEtape`),
-  INDEX `circuit_idx` (`idCircuit` ASC) ,
-  CONSTRAINT `circuit`
-    FOREIGN KEY (`idCircuit`)
-    REFERENCES `yassine`.`circuit` (`idCircuit`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_unicode_ci;
+-- --------------------------------------------------------
 
+--
+-- Structure de la table `activite`
+--
 
--- -----------------------------------------------------
--- Table `yassine`.`user`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `yassine`.`user` (
-  `idUser` INT(11) NOT NULL AUTO_INCREMENT,
-  `nomUser` VARCHAR(100) NOT NULL,
-  `prenomUser` VARCHAR(100) NOT NULL,
-  `sexe` CHAR(1) CHARACTER SET 'utf32' COLLATE 'utf32_unicode_ci' NOT NULL,
-  `dateNaissance` DATE NOT NULL,
-  `dateInscription` DATETIME NOT NULL,
-  `email` VARCHAR(100) NOT NULL,
-  `pwd` VARCHAR(100) NOT NULL,
-  `statut` BIT(1) NOT NULL DEFAULT b'1',
-  `type` ENUM('M', 'P', 'A') NOT NULL,
-  PRIMARY KEY (`idUser`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_unicode_ci;
+CREATE TABLE `activite` (
+  `idActivite` int(11) NOT NULL,
+  `titreActivite` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `villeActivite` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `descriptionActivite` text COLLATE utf8_unicode_ci NOT NULL,
+  `idJour` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+-- --------------------------------------------------------
 
--- -----------------------------------------------------
--- Table `yassine`.`historique`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `yassine`.`historique` (
-  `idUser` INT(11) NOT NULL,
-  `idCircuit` INT(11) NOT NULL,
-  `dateAchat` DATE NOT NULL,
-  `prixAchat` DOUBLE NOT NULL,
-  INDEX `idCircuit` (`idCircuit` ASC) ,
-  INDEX `idUser` (`idUser` ASC) ,
-  CONSTRAINT `historique_ibfk_1`
-    FOREIGN KEY (`idCircuit`)
-    REFERENCES `yassine`.`circuit` (`idCircuit`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-  CONSTRAINT `historique_ibfk_2`
-    FOREIGN KEY (`idUser`)
-    REFERENCES `yassine`.`user` (`idUser`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_unicode_ci
-ROW_FORMAT = COMPACT;
+--
+-- Structure de la table `circuit`
+--
 
+CREATE TABLE `circuit` (
+  `idCircuit` int(11) NOT NULL,
+  `titreCircuit` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `descriptionCircuit` text COLLATE utf8_unicode_ci NOT NULL,
+  `dateDebutCircuit` date NOT NULL,
+  `dateFinCircuit` date NOT NULL,
+  `prixCircuit` double NOT NULL,
+  `etat` bit(1) NOT NULL DEFAULT b'0',
+  `nbPlaceMin` int(11) NOT NULL,
+  `nbPlaceMax` int(11) NOT NULL,
+  `nbReservationMax` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- -----------------------------------------------------
--- Table `yassine`.`hotel`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `yassine`.`hotel` (
-  `idHotel` INT(11) NOT NULL AUTO_INCREMENT,
-  `nomHotel` VARCHAR(100) NOT NULL,
-  `villeHotel` VARCHAR(100) NOT NULL,
-  `lienHotel` VARCHAR(200) NOT NULL,
-  `idJour` INT(11) NOT NULL,
-  PRIMARY KEY (`idHotel`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_unicode_ci;
+-- --------------------------------------------------------
 
+--
+-- Structure de la table `circuit_etape`
+--
 
--- -----------------------------------------------------
--- Table `yassine`.`jour`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `yassine`.`jour` (
-  `idJour` INT(11) NOT NULL AUTO_INCREMENT,
-  `titreJour` VARCHAR(100) NOT NULL,
-  `descriptionJour` TEXT NOT NULL,
-  `idEtape` INT(11) NOT NULL,
-  PRIMARY KEY (`idJour`),
-  INDEX `etape_idx` (`idEtape` ASC) ,
-  CONSTRAINT `etape`
-    FOREIGN KEY (`idEtape`)
-    REFERENCES `yassine`.`etape` (`idEtape`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_unicode_ci;
+CREATE TABLE `circuit_etape` (
+  `idCircuit` int(11) NOT NULL,
+  `idEtape` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+-- --------------------------------------------------------
 
--- -----------------------------------------------------
--- Table `yassine`.`message`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `yassine`.`message` (
-  `idMessage` INT(11) NOT NULL AUTO_INCREMENT,
-  `titreMessage` VARCHAR(100) CHARACTER SET 'utf32' COLLATE 'utf32_unicode_ci' NOT NULL,
-  `texteMessage` TEXT NOT NULL,
-  `idUser` INT(11) NOT NULL,
-  PRIMARY KEY (`idMessage`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_unicode_ci;
+--
+-- Structure de la table `connexion`
+--
 
+CREATE TABLE `connexion` (
+  `email` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `pwd` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `statut` bit(1) NOT NULL DEFAULT b'1',
+  `type` enum('M','P','A') COLLATE utf8_unicode_ci NOT NULL,
+  `idUser` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- -----------------------------------------------------
--- Table `yassine`.`promotion`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `yassine`.`promotion` (
-  `idPromotion` INT(11) NOT NULL AUTO_INCREMENT,
-  `pourcentage` DOUBLE NOT NULL,
-  `titrePromotion` VARCHAR(100) NOT NULL,
-  `dateDebutPromotion` DATETIME NOT NULL,
-  `dateFinPromotion` DATETIME NOT NULL,
-  `idCircuit` INT(11) NOT NULL,
-  PRIMARY KEY (`idPromotion`),
-  INDEX `circuit_idx` (`idCircuit` ASC) ,
-  CONSTRAINT `circuit`
-    FOREIGN KEY (`idCircuit`)
-    REFERENCES `yassine`.`circuit` (`idCircuit`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_unicode_ci;
+-- --------------------------------------------------------
 
+--
+-- Structure de la table `etape`
+--
 
--- -----------------------------------------------------
--- Table `yassine`.`reservation`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `yassine`.`reservation` (
-  `idReservation` INT(11) NOT NULL AUTO_INCREMENT,
-  `idUser` INT(11) NOT NULL,
-  `idCircuit` INT(11) NOT NULL,
-  `dateReservation` DATETIME NOT NULL,
-  `depot` DOUBLE NOT NULL,
-  PRIMARY KEY (`idReservation`),
-  INDEX `user_idx` (`idUser` ASC) ,
-  INDEX `circuit_idx` (`idCircuit` ASC) ,
-  CONSTRAINT `user`
-    FOREIGN KEY (`idUser`)
-    REFERENCES `yassine`.`user` (`idUser`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `circuit`
-    FOREIGN KEY (`idCircuit`)
-    REFERENCES `yassine`.`circuit` (`idCircuit`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_unicode_ci;
+CREATE TABLE `etape` (
+  `idEtape` int(11) NOT NULL,
+  `titreEtape` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `descriptionEtape` text COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+-- --------------------------------------------------------
 
--- -----------------------------------------------------
--- Table `yassine`.`rabais`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `yassine`.`rabais` (
-  `idRabais` INT(11) NOT NULL AUTO_INCREMENT,
-  `montantRabais` DOUBLE NOT NULL,
-  `titreRabais` VARCHAR(100) NOT NULL,
-  `dateRabais` DATETIME NOT NULL,
-  `idReservation` INT(11) NOT NULL,
-  PRIMARY KEY (`idRabais`),
-  INDEX `reservation_idx` (`idReservation` ASC) ,
-  CONSTRAINT `reservation`
-    FOREIGN KEY (`idReservation`)
-    REFERENCES `yassine`.`reservation` (`idReservation`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_unicode_ci;
+--
+-- Structure de la table `historique`
+--
 
+CREATE TABLE `historique` (
+  `idUser` int(11) NOT NULL,
+  `idCircuit` int(11) NOT NULL,
+  `dateAchat` date NOT NULL,
+  `prixAchat` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci ROW_FORMAT=COMPACT;
 
--- -----------------------------------------------------
--- Table `yassine`.`restaurant`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `yassine`.`restaurant` (
-  `idResto` INT(11) NOT NULL AUTO_INCREMENT,
-  `nomResto` VARCHAR(100) NOT NULL,
-  `villeResto` VARCHAR(100) NOT NULL,
-  `lienResto` VARCHAR(200) CHARACTER SET 'utf32' COLLATE 'utf32_unicode_ci' NOT NULL,
-  `idJour` INT(11) NOT NULL,
-  PRIMARY KEY (`idResto`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_unicode_ci;
+-- --------------------------------------------------------
 
+--
+-- Structure de la table `hotel`
+--
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+CREATE TABLE `hotel` (
+  `idHotel` int(11) NOT NULL,
+  `nomHotel` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `villeHotel` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `lienHotel` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `idJour` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `jour`
+--
+
+CREATE TABLE `jour` (
+  `idJour` int(11) NOT NULL,
+  `titreJour` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `descriptionJour` text COLLATE utf8_unicode_ci NOT NULL,
+  `idEtape` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `message`
+--
+
+CREATE TABLE `message` (
+  `idMessage` int(11) NOT NULL,
+  `titreMessage` varchar(100) CHARACTER SET utf32 COLLATE utf32_unicode_ci NOT NULL,
+  `texteMessage` text COLLATE utf8_unicode_ci NOT NULL,
+  `idUser` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `promotion`
+--
+
+CREATE TABLE `promotion` (
+  `idPromotion` int(11) NOT NULL,
+  `pourcentage` double NOT NULL,
+  `titrePromotion` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `dateDebutPromotion` datetime NOT NULL,
+  `dateFinPromotion` datetime NOT NULL,
+  `idCircuit` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `rabais`
+--
+
+CREATE TABLE `rabais` (
+  `idRabais` int(11) NOT NULL,
+  `montantRabais` double NOT NULL,
+  `titreRabais` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `dateRabais` datetime NOT NULL,
+  `idUser` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `reservation`
+--
+
+CREATE TABLE `reservation` (
+  `idReservation` int(11) NOT NULL,
+  `idUser` int(11) NOT NULL,
+  `idCircuit` int(11) NOT NULL,
+  `dateReservation` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `restaurant`
+--
+
+CREATE TABLE `restaurant` (
+  `idResto` int(11) NOT NULL,
+  `nomResto` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `villeResto` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `lienResto` varchar(200) CHARACTER SET utf32 COLLATE utf32_unicode_ci NOT NULL,
+  `idJour` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+CREATE TABLE `user` (
+  `idUser` int(11) NOT NULL,
+  `nomUser` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `prenomUser` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `sexe` char(1) CHARACTER SET utf32 COLLATE utf32_unicode_ci NOT NULL,
+  `dateNaissance` date NOT NULL,
+  `dateInscription` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `activite`
+--
+ALTER TABLE `activite`
+  ADD PRIMARY KEY (`idActivite`);
+
+--
+-- Index pour la table `circuit`
+--
+ALTER TABLE `circuit`
+  ADD PRIMARY KEY (`idCircuit`);
+
+--
+-- Index pour la table `circuit_etape`
+--
+ALTER TABLE `circuit_etape`
+  ADD KEY `idCircuit` (`idCircuit`),
+  ADD KEY `idEtape` (`idEtape`);
+
+--
+-- Index pour la table `connexion`
+--
+ALTER TABLE `connexion`
+  ADD PRIMARY KEY (`email`),
+  ADD KEY `idUser` (`idUser`);
+
+--
+-- Index pour la table `etape`
+--
+ALTER TABLE `etape`
+  ADD PRIMARY KEY (`idEtape`);
+
+--
+-- Index pour la table `historique`
+--
+ALTER TABLE `historique`
+  ADD KEY `idCircuit` (`idCircuit`),
+  ADD KEY `idUser` (`idUser`);
+
+--
+-- Index pour la table `hotel`
+--
+ALTER TABLE `hotel`
+  ADD PRIMARY KEY (`idHotel`);
+
+--
+-- Index pour la table `jour`
+--
+ALTER TABLE `jour`
+  ADD PRIMARY KEY (`idJour`);
+
+--
+-- Index pour la table `message`
+--
+ALTER TABLE `message`
+  ADD PRIMARY KEY (`idMessage`);
+
+--
+-- Index pour la table `promotion`
+--
+ALTER TABLE `promotion`
+  ADD PRIMARY KEY (`idPromotion`);
+
+--
+-- Index pour la table `rabais`
+--
+ALTER TABLE `rabais`
+  ADD PRIMARY KEY (`idRabais`);
+
+--
+-- Index pour la table `reservation`
+--
+ALTER TABLE `reservation`
+  ADD PRIMARY KEY (`idReservation`);
+
+--
+-- Index pour la table `restaurant`
+--
+ALTER TABLE `restaurant`
+  ADD PRIMARY KEY (`idResto`);
+
+--
+-- Index pour la table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`idUser`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `activite`
+--
+ALTER TABLE `activite`
+  MODIFY `idActivite` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `circuit`
+--
+ALTER TABLE `circuit`
+  MODIFY `idCircuit` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `etape`
+--
+ALTER TABLE `etape`
+  MODIFY `idEtape` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `hotel`
+--
+ALTER TABLE `hotel`
+  MODIFY `idHotel` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `jour`
+--
+ALTER TABLE `jour`
+  MODIFY `idJour` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `message`
+--
+ALTER TABLE `message`
+  MODIFY `idMessage` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `promotion`
+--
+ALTER TABLE `promotion`
+  MODIFY `idPromotion` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `rabais`
+--
+ALTER TABLE `rabais`
+  MODIFY `idRabais` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `reservation`
+--
+ALTER TABLE `reservation`
+  MODIFY `idReservation` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `restaurant`
+--
+ALTER TABLE `restaurant`
+  MODIFY `idResto` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `user`
+--
+ALTER TABLE `user`
+  MODIFY `idUser` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `circuit_etape`
+--
+ALTER TABLE `circuit_etape`
+  ADD CONSTRAINT `circuit_etape_ibfk_1` FOREIGN KEY (`idCircuit`) REFERENCES `circuit` (`idCircuit`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `circuit_etape_ibfk_2` FOREIGN KEY (`idEtape`) REFERENCES `etape` (`idEtape`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `connexion`
+--
+ALTER TABLE `connexion`
+  ADD CONSTRAINT `connexion_ibfk_1` FOREIGN KEY (`idUser`) REFERENCES `user` (`idUser`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `historique`
+--
+ALTER TABLE `historique`
+  ADD CONSTRAINT `historique_ibfk_1` FOREIGN KEY (`idCircuit`) REFERENCES `circuit` (`idCircuit`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `historique_ibfk_2` FOREIGN KEY (`idUser`) REFERENCES `user` (`idUser`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
