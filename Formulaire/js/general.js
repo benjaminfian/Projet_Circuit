148a98eca185fddc9407c99ef89bdec891bfe8ee
yassine semmar <yassine3054@hotmail.com>
2019-04-24 15:01:58 -0400

    vhs

--- Formulaire/js/general.js
+++ Formulaire/js/general.js
@@ -1,6 +1,41 @@
-tinymce.init({
-    selector: 'textarea'
-});
+$(document).ready(function() {
+    tinymce.init({
+      selector: "textarea",
+      theme: "modern",
+      paste_data_images: true,
+      plugins: [
+        "advlist autolink lists link image charmap print preview hr anchor pagebreak",
+        "searchreplace wordcount visualblocks visualchars code fullscreen",
+        "insertdatetime media nonbreaking save table contextmenu directionality",
+        "emoticons template paste textcolor colorpicker textpattern"
+      ],
+      toolbar1: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
+      toolbar2: "print preview media | forecolor backcolor emoticons",
+      image_advtab: true,
+      file_picker_callback: function(callback, value, meta) {
+        if (meta.filetype == 'image') {
+          $('#upload').trigger('click');
+          $('#upload').on('change', function() {
+            var file = this.files[0];
+            var reader = new FileReader();
+            reader.onload = function(e) {
+              callback(e.target.result, {
+                alt: ''
+              });
+            };
+            reader.readAsDataURL(file);
+          });
+        }
+      },
+      templates: [{
+        title: 'Test template 1',
+        content: 'Test 1'
+      }, {
+        title: 'Test template 2',
+        content: 'Test 2'
+      }]
+    });
+  });
 
 
 var today = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate());
@@ -12,6 +47,7 @@ $('#startDate').datepicker({
         return $('#endDate').val();
     }
 });
+
 $('#endDate').datepicker({
     uiLibrary: 'bootstrap4',
     iconsLibrary: 'fontawesome',
@@ -19,3 +55,22 @@ $('#endDate').datepicker({
         return $('#startDate').val();
     }
 });
+
+$('#dateDebutPromotion').datepicker({
+    uiLibrary: 'bootstrap4',
+    iconsLibrary: 'fontawesome',
+    minDate: today,
+    maxDate: function () {
+        return $('#dateFinPromotion').val();
+    }
+});
+
+$('#dateFinPromotion').datepicker({
+    uiLibrary: 'bootstrap4',
+    iconsLibrary: 'fontawesome',
+    minDate: function () {
+        return $('#dateDebutPromotion').val();
+    }
+});
+
+
